7f7a27d4cf194158ebe69391dc1cbb600b74466e
leye <adeleyeifaturoti@yahoo.com>
2025-11-29 19:15:26 +0000

    rough Hero section done

--- components/Button.tsx
+++ components/Button.tsx
@@ -20,11 +20,12 @@ export default function Button({
   ...props
 }: ButtonProps) {
   const baseStyles =
-    "px-6 py-3 rounded-lg font-bold transition-all duration-200 inline-flex items-center justify-center";
+    "px-6 py-3 rounded-lg font-medium transition-all duration-200 inline-flex items-center justify-center";
 
   const variants = {
-    filled: "bg-white-600 text-white hover:bg-white-700 active:scale-95",
-    outline: "bg-transparent border-1  hover:bg-teal-500 active:scale-95",
+    filled: "bg-ld-teal text-black hover:bg-white-700 active:scale-95",
+    outline:
+      "bg-transparent border-gray-800 border-1 hover:bg-teal-500 active:scale-95",
   };
 
   const widthStyle = fullWidth ? "w-full" : "";
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,58 @@
-import React from "react";
+import Image from "next/image";
 import Aurora from "./Aurora";
+import Button from "./Button";
+
+const heroSerivces = [
+  "Up to 5 pages + mobile responsive",
+  "1 year hosting + domain included",
+  "SEO optimized + Web Analytics included",
+];
+
 const Hero = () => {
   return (
-    <div>
-      <div className="Hero_Image"></div>
-      <div className="Hero_Content">
-        <Aurora />
+    <div className="Hero flex flex-col md:flex-row-reverse items-center ">
+      <div className="Hero_Image">
+        <Image
+          src="images/hero-image.svg"
+          alt="Hero"
+          width={700}
+          height={700}
+          style={{ maxHeight: "70vh" }}
+        />
+      </div>
+      <div className="Hero_Content md:max-w-[50%]">
+        {/* <Aurora /> */}
+        <div className="Hero_MainContent flex flex-col gap-4">
+          <h2 className="font-medium text-[58px]">
+            Stunning, captivating websites delivered in just{" "}
+            <span className="text-ld-teal underline font-extrabold">
+              2 Weeks
+            </span>
+            .
+          </h2>
+          <p className="Hero_SubHeading text-2xl">
+            We handle the website so you can focus on what you do best
+          </p>
+          <div className="Hero_Offer">
+            <ul className="text-ld-teal">
+              {heroSerivces.map((service, index) => (
+                <li key={index} className="flex gap-2 text-white text-[20px]">
+                  <Image
+                    src={"images/hero-tick.svg"}
+                    width={20}
+                    height={20}
+                    alt="tick"
+                  />
+                  {service}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="Hero_Button flex gap-5">
+            <Button variant="filled">Book Free Discovery Call</Button>
+            <Button variant="outline">View Projects</Button>
+          </div>
+        </div>
       </div>
     </div>
   );
