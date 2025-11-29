c6a172659bcbd88bf7b1d81de614ec74b0073fc1
leye <adeleyeifaturoti@yahoo.com>
2025-11-29 15:06:36 +0000

    reverted background color

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
     <div>
-      <main></main>
+      <main>
+        <Hero />
+      </main>
     </div>
   );
 }
---
+++ components/Hero.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Aurora from "./Aurora";
+const Hero = () => {
+  return (
+    <div>
+      <div className="Hero_Image"></div>
+      <div className="Hero_Content">
+        <Aurora />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
