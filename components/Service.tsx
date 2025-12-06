a031d2fffdd306187bc109b1ce82ac96b614ee2a
leye <adeleyeifaturoti@yahoo.com>
2025-12-06 16:57:08 +0000

    removed Service spread in ServiceList.tsx and imported Service as ServiceProps in Service.tsx

--- components/Service.tsx
+++ components/Service.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
-import type { Service } from "../data/services";
+import type { Service as ServiceProps } from "../data/services";
 
-const Service = ({ service, description, imgUrl }: Service) => {
+const Service = ({ service, description, imgUrl }: ServiceProps) => {
   return (
     <div className="Service border-b-2 border-gray-500 hover:border-ld-teal flex flex-col gap-4 items-center m-3">
       <Image src={imgUrl} alt={service} width={42} height={42} />
--- components/ServiceList.tsx
+++ components/ServiceList.tsx
@@ -4,8 +4,13 @@ import Service from "./Service";
 const ServiceList = () => {
   return (
     <div className="ServiceList grid grid-cols-2 md:grid-cols-3 gap-5 md:gap-10 my-10">
-      {services.map((service) => (
-        <Service key={service.service} {...service} />
+      {services.map(({ service, description, imgUrl }) => (
+        <Service
+          key={service}
+          imgUrl={imgUrl}
+          service={service}
+          description={description}
+        />
       ))}
     </div>
   );
