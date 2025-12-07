eb0e6c6d7a7612eaeeb80a67e6977ee7b9679579
leye <adeleyeifaturoti@yahoo.com>
2025-12-07 17:10:47 +0000

    optimise Hero image and Services for 4 cols on large

---
+++ components/Benefits.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Benefits = () => {
+  return (
+    <div className="Benefits">
+      <h3 className="Benefits_Heading">BENEFITS</h3>
+    </div>
+  );
+};
+
+export default Benefits;
