701af316350c43e892a7ab576a3424238cb03800
leye <adeleyeifaturoti@yahoo.com>
2026-01-05 20:58:09 +0000

    added some important aspects of pricing

--- components/PricingList.tsx
+++ components/PricingList.tsx
@@ -1,7 +1,45 @@
 import React from "react";
+import PricingCard from "./cards/PricingCard";
+
+const prices = [
+  {
+    title: "2 - Week Website Package",
+    price: "£1,000",
+    description: "Complete Professional Website Solution",
+    features: [
+      "Up to 5 Professional Pages/Sections",
+      "1 year year hosting included",
+      "Basic SEO Optimization",
+      "Professional email address",
+      "Social Media Integration",
+      "Security & SSL Certificate",
+      "Fully Responsive Design",
+      "1 year domain included",
+      "Website Analytics",
+      "Contact form",
+      "5 Total Revisions",
+      "Mobile Optimization",
+      "Search Console Registration",
+    ],
+    button: "Book Free Discovery Call",
+  },
+];
 
 const PricingList = () => {
-  return <div>PricingList</div>;
+  return (
+    <div>
+      {prices.map(({ title, description, price, features, button }, index) => (
+        <PricingCard
+          key={index}
+          title={title}
+          description={description}
+          price={price}
+          features={features}
+          button={button}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default PricingList;
--- components/cards/PricingCard.tsx
+++ components/cards/PricingCard.tsx
@@ -1,21 +1,43 @@
+import Image from "next/image";
+
 type PriceProp = {
   title: string;
   description: string;
   price: string;
   features: string[];
   button: string;
-  isRecomended?: boolean;
+  isRecommended?: boolean;
 };
 
 const PricingCard = ({ title, description, price, features }: PriceProp) => {
   return (
-    <div className="PricingCard">
-      <h4 className="PricingCard_Title">{title}</h4>
-      <p className="PricingCard_Description">{description}</p>
-      <h4 className="PricingCard_Price">£{price}</h4>
-      <ul className="PricingCard_List">
+    <div className="PricingCard ">
+      <h4 className="PricingCard_Title text-[32px] font-bold text-center">
+        {title}
+      </h4>
+      <p className="PricingCard_Description text-[20px] text-center">
+        {description}
+      </p>
+      <h4 className="PricingCard_Price text-ld-teal text-[63px] text-center font-extrabold">
+        {price}
+      </h4>
+      <p className="PricingCard_TopSub text-center text-[14px] text-gray-400">
+        Complete package . No Hidden fees
+      </p>
+      <ul className="PricingCard_List grid grid-cols-2 gap-5">
         {features.map((feature, index) => (
-          <li key={index}>{feature}</li>
+          <li
+            className="PricingCard_Feature flex gap-2 text-[20px]"
+            key={index}
+          >
+            <Image
+              src="/images/pricing-tick.svg"
+              alt="tick"
+              width={24}
+              height={24}
+            />
+            {feature}
+          </li>
         ))}
       </ul>
     </div>
